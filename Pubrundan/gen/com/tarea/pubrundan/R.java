7155be8649962821aba140fcc93d042c7edcc6c7
gitsjogren <eriksjogren91@gmail.com>
2012-09-23 23:47:30 +0200

    Inlämning #02 - DONE!

--- Pubrundan/gen/com/tarea/pubrundan/R.java
+++ Pubrundan/gen/com/tarea/pubrundan/R.java
@@ -13,18 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int pushpin=0x7f020002;
     }
     public static final class id {
-        public static final int edit_message=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int edit_message=0x7f060000;
+        public static final int mapView=0x7f060001;
+        public static final int zoom=0x7f060002;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
-    }
-    public static final class menu {
-        public static final int activity_display_message=0x7f060000;
-        public static final int activity_main=0x7f060001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
