2f1039c960a4c68cf8e4ca5f2faa2096a5239849
gitsjogren <eriksjogren91@gmail.com>
2012-10-08 00:46:35 +0200

    Inlämning #04
    - Källkod
    - Release (features, apk, buggar, versionnummer)
    - Dokumenterad sprint planning
    - Sprint backlog
    - Product backlog
    - Dokumenterad Definition-Of-Done
    - Påbörjad användarmanual
    - Utvecklarmanual

--- Pubrundan/gen/com/tarea/pubrundan/R.java
+++ Pubrundan/gen/com/tarea/pubrundan/R.java
@@ -10,45 +10,64 @@ package com.tarea.pubrundan;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int green=0x7f04000c;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int prippsblue=0x7f040010;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int icon_change_map=0x7f020002;
         public static final int icon_list_view=0x7f020003;
-        public static final int icon_location_search=0x7f020004;
-        public static final int icon_map=0x7f020005;
-        public static final int icon_settings=0x7f020006;
-        public static final int icon_share=0x7f020007;
-        public static final int pushpin=0x7f020008;
+        public static final int icon_location_found=0x7f020004;
+        public static final int icon_location_search=0x7f020005;
+        public static final int icon_map=0x7f020006;
+        public static final int icon_settings=0x7f020007;
+        public static final int icon_share=0x7f020008;
+        public static final int pushpin=0x7f020009;
     }
     public static final class id {
-        public static final int change_map=0x7f070004;
+        public static final int backToMap=0x7f070008;
+        public static final int changeCampus=0x7f070007;
+        public static final int change_map=0x7f070003;
         public static final int getLocationButton=0x7f070000;
+        public static final int getPosition=0x7f070006;
         public static final int listViewButton=0x7f070001;
-        public static final int mapView=0x7f070002;
-        public static final int settings=0x7f070005;
-        public static final int share=0x7f070006;
-        public static final int zoom=0x7f070003;
+        public static final int mapshow_button=0x7f070002;
+        public static final int mv=0x7f070009;
+        public static final int pubList=0x7f07000a;
+        public static final int settings=0x7f070004;
+        public static final int share=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int main=0x7f030001;
         public static final int menu=0x7f030002;
+        public static final int pubinfo=0x7f030003;
+        public static final int publist=0x7f030004;
+        public static final int showthemap=0x7f030005;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int changeCampus_button=0x7f040002;
-        public static final int edit_message=0x7f040001;
-        public static final int getLocation=0x7f040006;
-        public static final int listview=0x7f040007;
-        public static final int map_of_johanneberg_display_message=0x7f040005;
-        public static final int menu_settings=0x7f040003;
-        public static final int title_activity_main=0x7f040004;
-    }
-    public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int goLabel=0x7f050002;
+        public static final int hello=0x7f050000;
     }
 }
