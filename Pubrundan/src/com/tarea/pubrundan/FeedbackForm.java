cf4458743db3b005e2a8bceb6be7af8741fd1026
edfast <rickard.edfast@gmail.com>
2012-10-10 00:39:19 +0200

    Börjat på ett feedbackformulär

---
+++ Pubrundan/src/com/tarea/pubrundan/FeedbackForm.java
@@ -0,0 +1,22 @@
+package com.tarea.pubrundan;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+
+public class FeedbackForm extends Activity{
+
+}
+
+//public void sendFeedback(View button){
+//	
+//	final EditText nameField = (EditText) findViewById(R.id.EditTextName);  
+//	String name = nameField.getText().toString();  
+//	final EditText emailField = (EditText) findViewById(R.id.EditTextEmail);  
+//	String email = emailField.getText().toString();  
+//	final EditText feedbackField = (EditText) findViewById(R.id.EditTextFeedbackBody);  
+//	String feedback = feedbackField.getText().toString();  
+//	
+//	final Spinner feedbackSpinner = (Spinner) findViewById(R.id.SpinnerFeedbackType);  
+//	String feedbackType = feedbackSpinner.getSelectedItem().toString(); 
+//}
