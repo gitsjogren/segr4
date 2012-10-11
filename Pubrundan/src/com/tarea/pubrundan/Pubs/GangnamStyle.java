25988e8effd5aff970221e2e7ad6850de19f7602
gitsjogren <eriksjogren91@gmail.com>
2012-10-12 00:48:21 +0200

    Git crash, worst ever.
    Hopefully not all files is lost...

---
+++ Pubrundan/src/com/tarea/pubrundan/Pubs/GangnamStyle.java
@@ -0,0 +1,53 @@
+/* ============================================================
+ * Copyright © 2012 Adam Book, Rickard Edfast, Erik Sjögren,
+ * Alexander Kurkimäki
+ *
+ * This file is part of Pubrundan.
+ *
+ * Pubrundan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pubrundan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pubrundan. If not, see <http://www.gnu.org/licenses/>.
+ * ============================================================ */
+
+package com.tarea.pubrundan.Pubs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.tarea.pubrundan.R;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class GangnamStyle.
+ */
+public class GangnamStyle extends Activity {
+	
+	
+	
+	// standard onCreate method
+		/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	public void onCreate(Bundle savedInstanceState) {
+
+			super.onCreate(savedInstanceState);
+			requestWindowFeature(Window.FEATURE_NO_TITLE); // Suppress title bar for
+															// more space
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			setContentView(R.layout.gangnamstyle);
+		
+	}
+
+}
