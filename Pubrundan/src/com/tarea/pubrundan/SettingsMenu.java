ab68e368abe7ac6cfd55b5d73b9662a41ede6318
edfast <rickard.edfast@gmail.com>
2012-10-17 16:23:03 +0200

    basic merge

--- Pubrundan/src/com/tarea/pubrundan/SettingsMenu.java
+++ Pubrundan/src/com/tarea/pubrundan/SettingsMenu.java
@@ -1,45 +1,91 @@
 package com.tarea.pubrundan;
 
-import android.content.*;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.Window;
 
-/*
+/**
  * The settings-menu where you're able to change your default
  * campus, send feedback on the application, see information about
  * the application itself and other features.
  */
 
+
 public class SettingsMenu extends PreferenceActivity {
 
-	/*
+
+	/**
 	 * (non-Javadoc)
+	 * 
 	 * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
-		SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		final SharedPreferences SP = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
 
-		/*
-		 * Get all options in res/xml/settings.xml
+		/**
+		 * A clickable "preference" which starts the About-section within
+		 * the SettingsMenu.
 		 */
-		String defaultCampus = SP.getString("defaultCampus", "true");
-		
-		Preference feedback = (Preference) findPreference("feedback");
-		feedback.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-			public boolean onPreferenceClick(Preference preference) {
-				Intent i = new Intent(getApplicationContext(), FeedbackForm.class);
-				startActivity(i);
+		Preference aboutPage = (Preference) findPreference("aboutPage");
+		aboutPage
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+					public boolean onPreferenceClick(Preference preference) {
+						Intent AboutActivity = new Intent(getBaseContext(),
+								AboutPage.class);
+						startActivity(AboutActivity);
+						return true;
+					}
+
+				});
+
+
+		/**
+		 * When the value is changed in the Default Campus settings the new value 
+		 * is stored in the SharedPreferences to be reminded each time you start
+		 * the application.
+		 */
+		Preference defaultCampus = (Preference) findPreference("defaultCampus");
+		defaultCampus.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+				SharedPreferences.Editor editor = SP.edit();
+				editor.putString("defaultCampus", (String) newValue);
+				editor.commit();
 				return true;
 			}
+
 		});
+			
+		Preference feedback = (Preference) findPreference("feedback");
+		feedback.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			
+			public boolean onPreferenceClick(Preference preference) {
+					Intent i = new Intent(getApplicationContext(), FeedbackForm.class);
+					startActivity(i);
+					return true;
+				}
+		});
+		/**
+		 * Get all options in res/xml/settings.xml
+		 */
+		SP.getString("defaultCampus", "0");
+		SP.getString("aboutPage", "");
+		SP.getString("feedback", "");
+
 	}
-	
 
 }
