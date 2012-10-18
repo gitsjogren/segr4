086029c74da26bf54242e3e0948484171521b82a
Erik Sjögren <eriksjogren91@gmail.com>
2012-10-18 12:40:19 +0200

    removed another class, one generic class works!
    some more refactoring.

--- Pubrundan/src/com/tarea/pubrundan/OverlayClass.java
+++ Pubrundan/src/com/tarea/pubrundan/OverlayClass.java
@@ -115,7 +115,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 		switch (counter.get(i)) {
 
 		case 0:		// Start J.A. Pripps activity
-			Intent jaPripps = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent jaPripps = new Intent(mContext, PubLayout.class);
 			jaPripps.putExtra("Pub layout", R.layout.japripps);
 			jaPripps.putExtra("Pub title", "J.A. Pripps");
 			jaPripps.putExtra("More info", R.string.ja_pripps_info);
@@ -123,7 +123,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(jaPripps);
 			return true;
 		case 1:		// Start Gasquen activity
-			Intent gasquen = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent gasquen = new Intent(mContext, PubLayout.class);
 			gasquen.putExtra("Pub layout", R.layout.gasquen);
 			gasquen.putExtra("Pub title", "Gasquen");
 			gasquen.putExtra("More info", R.string.ja_pripps_info);
@@ -131,7 +131,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(gasquen);
 			return true;
 		case 2:		// Start Bulten activity
-			Intent bulten = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent bulten = new Intent(mContext, PubLayout.class);
 			bulten.putExtra("Pub layout", R.layout.bulten);
 			bulten.putExtra("Pub title", "Bulten");
 			bulten.putExtra("More info", R.string.ja_pripps_info);
@@ -139,7 +139,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(bulten);
 			return true;
 		case 3:		// Start Winden activity
-			Intent winden = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent winden = new Intent(mContext, PubLayout.class);
 			winden.putExtra("Pub layout", R.layout.winden);
 			winden.putExtra("Pub title", "Winden");
 			winden.putExtra("More info", R.string.ja_pripps_info);
@@ -147,7 +147,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(winden);
 			return true;
 		case 4:		// Start Zaloonen Pripps activity
-			Intent zaloonen = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent zaloonen = new Intent(mContext, PubLayout.class);
 			zaloonen.putExtra("Pub layout", R.layout.zaloonen);
 			zaloonen.putExtra("Pub title", "Zaloonen");
 			zaloonen.putExtra("More info", R.string.ja_pripps_info);
@@ -155,7 +155,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(zaloonen);
 			return true;
 		case 5:		// Start Club Avancez activity
-			Intent clubAvancez = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent clubAvancez = new Intent(mContext, PubLayout.class);
 			clubAvancez.putExtra("Pub layout", R.layout.clubavancez);
 			clubAvancez.putExtra("Pub title", "Club Avancez");
 			clubAvancez.putExtra("More info", R.string.ja_pripps_info);
@@ -163,7 +163,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(clubAvancez);
 			return true;
 		case 6:		// Start GoldenI activity
-			Intent goldenI = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent goldenI = new Intent(mContext, PubLayout.class);
 			goldenI.putExtra("Pub layout", R.layout.goldeni);
 			goldenI.putExtra("Pub title", "GoldenI");
 			goldenI.putExtra("More info", R.string.ja_pripps_info);
@@ -171,7 +171,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(goldenI);
 			return true;
 		case 7:		// Start Hubben 2.1 activity
-			Intent hubben = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent hubben = new Intent(mContext, PubLayout.class);
 			hubben.putExtra("Pub layout", R.layout.hubben);
 			hubben.putExtra("Pub title", "Hubben 2.1");
 			hubben.putExtra("More info", R.string.ja_pripps_info);
@@ -179,7 +179,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(hubben);
 			return true;
 		case 8:		// Start Basen activity
-			Intent basen = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent basen = new Intent(mContext, PubLayout.class);
 			basen.putExtra("Pub layout", R.layout.basen);
 			basen.putExtra("Pub title", "Basen");
 			basen.putExtra("More info", R.string.ja_pripps_info);
@@ -187,7 +187,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(basen);
 			return true;
 		case 9:		// Start Kajsabaren activity
-			Intent kajsaBaren = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent kajsaBaren = new Intent(mContext, PubLayout.class);
 			kajsaBaren.putExtra("Pub layout", R.layout.kajsabaren);
 			kajsaBaren.putExtra("Pub title", "Kajsabaren");
 			kajsaBaren.putExtra("More info", R.string.ja_pripps_info);
@@ -195,7 +195,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(kajsaBaren);
 			return true;
 		case 10:	// Start Järnvägspub activity
-			Intent jarnvagsPub = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent jarnvagsPub = new Intent(mContext, PubLayout.class);
 			jarnvagsPub.putExtra("Pub layout", R.layout.jarnvagspub);
 			jarnvagsPub.putExtra("Pub title", "Järnvägspub");
 			jarnvagsPub.putExtra("More info", R.string.ja_pripps_info);
@@ -203,7 +203,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(jarnvagsPub);
 			return true;
 		case 11:	// Start GasTown activity
-			Intent gasTown = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent gasTown = new Intent(mContext, PubLayout.class);
 			gasTown.putExtra("Pub layout", R.layout.gastown);
 			gasTown.putExtra("Pub title", "GasTown");
 			gasTown.putExtra("More info", R.string.ja_pripps_info);
@@ -211,7 +211,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(gasTown);
 			return true;
 		case 12:	// Start FortNOx activity
-			Intent fortNox = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent fortNox = new Intent(mContext, PubLayout.class);
 			fortNox.putExtra("Pub layout", R.layout.fortnox);
 			fortNox.putExtra("Pub title", "FortNOx");
 			fortNox.putExtra("More info", R.string.ja_pripps_info);
@@ -219,7 +219,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(fortNox);
 			return true;
 		case 13:	// Start Spritköket activity
-			Intent spritkoket = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent spritkoket = new Intent(mContext, PubLayout.class);
 			spritkoket.putExtra("Pub layout", R.layout.spritkoket);
 			spritkoket.putExtra("Pub title", "Spritköket");
 			spritkoket.putExtra("More info", R.string.ja_pripps_info);
@@ -227,7 +227,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(spritkoket);
 			return true;
 		case 14:	// Start Focus activity
-			Intent focus = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent focus = new Intent(mContext, PubLayout.class);
 			focus.putExtra("Pub layout", R.layout.focus);
 			focus.putExtra("Pub title", "Focus");
 			focus.putExtra("More info", R.string.ja_pripps_info);
@@ -235,7 +235,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(focus);
 			return true;
 		case 15:	// Start Röda rummet activity
-			Intent rodaRummet = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent rodaRummet = new Intent(mContext, PubLayout.class);
 			rodaRummet.putExtra("Pub layout", R.layout.rodarummet);
 			rodaRummet.putExtra("Pub title", "Röda rummet");
 			rodaRummet.putExtra("More info", R.string.ja_pripps_info);
@@ -243,7 +243,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(rodaRummet);
 			return true;
 		case 16:	// Start Sigurd/A-fiket activity
-			Intent sigurdAfiket = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent sigurdAfiket = new Intent(mContext, PubLayout.class);
 			sigurdAfiket.putExtra("Pub layout", R.layout.sigurdafiket);
 			sigurdAfiket.putExtra("Pub title", "Sigurd/A-fiket");
 			sigurdAfiket.putExtra("More info", R.string.ja_pripps_info);
@@ -251,7 +251,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(sigurdAfiket);
 			return true;
 		case 17:	// Start Pub P activity
-			Intent pubP = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent pubP = new Intent(mContext, PubLayout.class);
 			pubP.putExtra("Pub layout", R.layout.pubp);
 			pubP.putExtra("Pub title", "Pub P");
 			pubP.putExtra("More info", R.string.ja_pripps_info);
@@ -259,7 +259,7 @@ public class OverlayClass extends ItemizedOverlay<OverlayItem> {
 			mContext.startActivity(pubP);
 			return true;
 		case 18:	// Start 11:an activity
-			Intent pubF = new Intent(mContext, PassingDataToPubLayout.class);
+			Intent pubF = new Intent(mContext, PubLayout.class);
 			pubF.putExtra("Pub layout", R.layout.pubf);
 			pubF.putExtra("Pub title", "Pub F");
 			pubF.putExtra("More info", R.string.ja_pripps_info);
